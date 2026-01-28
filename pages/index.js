e16b15f4db8b59314a74b0892d7dd356467ba572
Ammar Khan <57397520+ammarkhanone@users.noreply.github.com>
2026-01-28 20:10:51 +0500

    Add pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '40px', textAlign: 'center' }}>
+      <h1>nextjs-inte</h1>
+      <p>create the hello world app with some attractive UI</p>
+    </main>
+  )
+}
